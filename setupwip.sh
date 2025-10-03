99850d18448748576a883d93c7fad9a8011eaa1b
rabithoy <emailforfrom@gmail.com>
2025-10-03 23:17:48 +0700

    Update setupwip.sh

--- setupwip.sh
+++ setupwip.sh
@@ -21,9 +21,6 @@ sudo swapon "$SWAP_FILE" 2>/dev/null || true
 # Ghi vào /etc/fstab nếu chưa có
 grep -q "$SWAP_FILE" /etc/fstab || echo "$SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab
 # ss
-if [ ! -f "main.zip" ]; then
-  wget -O main.zip https://github.com/rabithoy/tth/raw/a7ef3df05ba3e835133506490849cc3750f8aaea/main.zip
-fi
 # Tối ưu kernel
 echo "🛠️ Setting sysctl options..."
 sudo sysctl vm.swappiness=10 vm.vfs_cache_pressure=50
@@ -33,13 +30,14 @@ sudo sysctl -p
 if [ ! -f "main.zip" ]; then
   wget -O main.zip https://github.com/rabithoy/tth/raw/a7ef3df05ba3e835133506490849cc3750f8aaea/main.zip
 fi
-    rm -rf 1.sh 2.sh 3.sh
-    wget https://raw.githubusercontent.com/rabithoy/bart/main/1.sh
-    wget -O 2.sh https://raw.githubusercontent.com/rabithoy/bart/main/2.sh
-    wget https://raw.githubusercontent.com/rabithoy/bart/main/3.sh
+#111
+rm -rf 1.sh 2.sh 3.sh
+wget https://raw.githubusercontent.com/rabithoy/bart/main/1.sh
+wget -O 2.sh https://raw.githubusercontent.com/rabithoy/bart/main/2.sh
+wget https://raw.githubusercontent.com/rabithoy/bart/main/3.sh
 
-    # Cấp quyền thực thi cho cả 3 file
-    chmod +x 1.sh 2.sh 3.sh
+# Cấp quyền thực thi cho cả 3 file
+chmod +x 1.sh 2.sh 3.sh
    # nohup bash ./3.sh >/dev/null 2>&1 &
 # 🧩 Bước 2: Giải nén đè
 unzip -o main.zip

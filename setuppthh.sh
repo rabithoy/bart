8b2de87d41d56b5b4cd78d87eb5947f8d223f49e
rabithoy <emailforfrom@gmail.com>
2025-10-06 17:56:33 +0700

    Update setuppthh.sh

--- setuppthh.sh
+++ setuppthh.sh
@@ -38,11 +38,9 @@ cd InternetIncome-main
 #
 sudo sed -i "s|^USE_PROXIES=.*|USE_PROXIES=true|" properties.conf
 # Thay REPOCKET_EMAIL và REPOCKET_API
-sudo sed -i 's|^REPOCKET_EMAIL=.*|REPOCKET_EMAIL=minshousevn@gmail.com|' properties.conf
-sudo sed -i 's|^REPOCKET_API=.*|REPOCKET_API=69b5f8b8-40d4-4586-9247-4aa27e48ccfe|' properties.conf
-
 sudo sed -i 's|^TRAFFMONETIZER_TOKEN=.*|TRAFFMONETIZER_TOKEN=Mu3hefwR2XsEoo3K+Kn+yFICzbJgNvdjezTN2FjrGIQ=|' properties.conf
 sudo sed -i "s|^CASTAR_SDK_KEY=.*|CASTAR_SDK_KEY=cskLE50HncydFo|" properties.conf
+sudo sed -i 's|--restart=always|--restart=no|g' internetIncome.sh
 
 AUTH_CODE=$(curl -s "http://54.36.60.95:9876/get-auth" | jq -r '.auth_code')
 sudo sed -i "s|^UR_AUTH_TOKEN=.*|UR_AUTH_TOKEN='$AUTH_CODE'|" properties.conf

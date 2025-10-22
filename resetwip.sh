0d31627d3963072c2d6f76fee47d836340179027
rabithoy <emailforfrom@gmail.com>
2025-10-22 14:11:38 +0700

    Update resetwip.sh

--- resetwip.sh
+++ resetwip.sh
@@ -2,8 +2,7 @@
 
 while true; do
   # Lấy danh sách tất cả container có prefix wipter, traff hoặc caster
-  CONTAINERS=$(sudo docker ps --format '{{.Names}}' | grep -E '^(traff|proxyrack
-  wipter)' | sort)
+  CONTAINERS=$(sudo docker ps --format '{{.Names}}' | grep -E '^(traff|proxyrack|wipter)' | sort)
 
   for cname in $CONTAINERS; do
       echo "Restarting $cname at $(date)"
